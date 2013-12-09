be8acb4bd0bba2d9a06984d2f7408c349b91d387
Gisselle Encalada <gisse.encalada@gmail.com>
2013-12-08 22:14:06 -0300

    modified taskActivity and tasksActivity

--- src/com/example/examen810_db/TaskActivity.java
+++ src/com/example/examen810_db/TaskActivity.java
@@ -3,12 +3,6 @@ package com.example.examen810_db;
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.DatePicker;
-import android.widget.Toast;
-
 import com.example.examen.R;
 
 public class TaskActivity extends ListActivity {
@@ -19,32 +13,13 @@ public class TaskActivity extends ListActivity {
 	public static final String EXTRA_TASK = "task";
 	public static final String EXTRA_REMOVE = "remove";
 	
-	DatePicker dia;
-	Button boton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_task);
 		
-		dia =(DatePicker)findViewById(R.id.dpDia);
-		boton = (Button)findViewById(R.id.btButton);
-		
-		boton.setOnClickListener(new OnClickListener(){
-	
 
-			@Override
-			public void onClick(View view) {
-				// TODO Auto-generated method stub
-				final int mes = dia.getMonth();
-				final int uno = 1;
-				Toast.makeText(getBaseContext(), "El dia seleccionado es:"+
-					(mes + uno) + "/" + dia.getDayOfMonth() +
-					"/" + dia.getYear(), Toast.LENGTH_LONG).show();
-			}
-			
-		
-		});
 		
 	}
 
--- src/com/example/examen810_db/TasksActivity.java
+++ src/com/example/examen810_db/TasksActivity.java
@@ -4,13 +4,37 @@ import com.example.examen.R;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Toast;
 
 public class TasksActivity extends Activity {
-
+	DatePicker dia;
+	Button boton;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_tasks);
+		
+		dia =(DatePicker)findViewById(R.id.dpDia);
+		boton = (Button)findViewById(R.id.btButton);
+		
+		boton.setOnClickListener(new OnClickListener(){	
+
+			@Override
+			public void onClick(View view) {
+				// TODO Auto-generated method stub
+				final int mes = dia.getMonth();
+				final int uno = 1;
+				Toast.makeText(getBaseContext(), "El dia seleccionado es:"+
+					(mes + uno) + "/" + dia.getDayOfMonth() +
+					"/" + dia.getYear(), Toast.LENGTH_LONG).show();
+			}
+					
+		});
 	}
 
 	@Override
