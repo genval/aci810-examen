ab031d73dd531152f04c49bce2582bc600300ab5
Gisselle Encalada <gisse.encalada@gmail.com>
2013-12-09 22:05:17 -0300

    conf tasksActivity

--- src/com/example/examen810_db/TasksActivity.java
+++ src/com/example/examen810_db/TasksActivity.java
@@ -6,7 +6,9 @@ import com.example.examen.R;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.support.v4.app.NavUtils;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -105,6 +107,58 @@ public class TasksActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.tasks, menu);
 		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			// This ID represents the Home or Up button. In the case of this
+			// activity, the Up button is shown. Use NavUtils to allow users
+			// to navigate up one level in the application structure. For
+			// more details, see the Navigation pattern on Android Design:
+			//
+			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
+			//
+			NavUtils.navigateUpFromSameTask(this);
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	public void onSaveButtonClicked(View view) {
+		EditText taskName = (EditText) this.findViewById(R.id.nameTask);
+		String nameTask = taskName.getText().toString();
+		
+		EditText descriptionTask = (EditText) this.findViewById(R.id.descriptionTask);
+		String taskDescription = descriptionTask.getText().toString();
+		
+		DatePicker dateField = (DatePicker) this.findViewById(R.id.dpDia);
+		//int date = dateField.get;
+		
+		if(nameTask.isEmpty() || taskDescription.isEmpty() || date.isEmpty())
+		{
+			Toast.makeText(this, "Complete the form before saving", Toast.LENGTH_LONG).show();
+			return;
+		}
+		
+		Task t = null;
+		
+		if(this.taskToUpdate != null)
+		{
+			t = ds.updateTask(this.taskToUpdate, nameTask, taskDescription, date);
+		}
+		else
+		{
+			t = ds.createTask(nameTask, taskDescription, date);
+		}
+		
+		Intent i = new Intent();
+		i.putExtra(TaskActivity.EXTRA_TASK, t);
+		i.putExtra(TaskActivity.EXTRA_REMOVE, false);
+		this.setResult(RESULT_OK, i);
+		
+		this.finish();
 	}
 	
 	public void onDeleteButtonClicked(View view) {
